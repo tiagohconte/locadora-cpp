29615ba3b65a02175e08a1fed236baf3b1174c51
Tiago Conte <tiagohconte@gmail.com>
2023-02-08 19:30:49 -0300

    Adiciona atributos portas e categoria à classe Carro

---
+++ Carro.cpp
@@ -0,0 +1,23 @@
+#include "Carro.hpp"
+
+namespace locadora {
+
+Carro::Carro(Marca* const marca, const std::string& modelo, const float custo,
+             const unsigned short int capacidade, const Placa& placa,
+             const unsigned short int portas, const EnumCategoria categoria)
+    : Veiculo{marca, modelo, custo, capacidade, placa},
+      portas{portas},
+      categoria{categoria} {}
+
+unsigned short int Carro::getPortas() const { return this->portas; }
+
+void Carro::setPortas(const unsigned short int portas) {
+    this->portas = portas;
+}
+
+EnumCategoria Carro::getCategoria() const { return this->categoria; }
+
+void Carro::setCategoria(const EnumCategoria categoria) {
+    this->categoria = categoria;
+}
+}  // namespace locadora
--- Carro.hpp
+++ Carro.hpp
@@ -4,12 +4,27 @@
 #include "Veiculo.hpp"
 
 namespace locadora {
+
+enum class EnumCategoria { SEDAN, HATCH, SUV };
+
 class Carro : public Veiculo {
-    public:
-        Carro();
-        virtual ~Carro() = default;
-    private:
+   public:
+    Carro(Marca* const marca, const std::string& modelo, const float custo,
+          const unsigned short int capacidade, const Placa& placa,
+          const unsigned short int portas, const EnumCategoria categoria);
+
+    virtual ~Carro() = default;
+
+    unsigned short int getPortas() const;
+    void setPortas(const unsigned short int portas);
+
+    EnumCategoria getCategoria() const;
+    void setCategoria(const EnumCategoria categoria);
+
+   private:
+    unsigned short int portas;
+    EnumCategoria categoria;
 };
-}
+}  // namespace locadora
 
 #endif
